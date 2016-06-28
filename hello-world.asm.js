025d3d3703fbfe9f0334b6569daada3334d89fa1
Sandesh <sandeshdamkondwar@gmail.com>
2016-06-28 16:30:58 +0530

    Initial commit

---
+++ hello-world.asm.js
@@ -0,0 +1,11 @@
+function MyMathModule(global) {
+    "use asm";
+    var exp = global.Math.exp;
+    function doubleExp(value) {
+        value = +value;
+        return +(+exp(+value) * 2.0);
+    }
+    return {
+        doubleExp: doubleExp
+    };
+}
